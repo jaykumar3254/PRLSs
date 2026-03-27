04afe20e4412190cad21e802de6ed4b67545ff29
JayKumar <pardeshijaikumar@gmail.com>
2026-03-27 20:00:31 +0530

    comp1

--- PrlssInterface/src/Contexts/User.jsx
+++ PrlssInterface/src/Contexts/User.jsx
@@ -1,6 +1,23 @@
-function User(){
-    return <div>User COntext</div>;
+import { Children, createContext, useState } from "react";
+import axios from 'react';
+const userContext = createContext();
 
-}
+function provider({Children}){
 
-export default User;
+    const [user,setUser] = useState([]);
+
+    const createUser = async (title) => {
+        const ressponse = await axios.post('http://localhost:3001/book',{
+            title,
+        });
+        setUser([...user,ressponse.data]);
+    }
+    
+    return( <userContext.Provider value={{user,createUser}} >
+    {Children}
+    </userContext.Provider>);
+
+};
+
+export {provider};
+export default userContext;
